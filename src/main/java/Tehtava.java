7719d2feb14e8d46f1bcda4dc21bb1f5054c3da7
Mikko <mikko.syrjala94@hotmail.com>
2020-03-22 18:39:19 +0200

    Pientä fiksailua

--- src/main/java/Tehtava.java
+++ src/main/java/Tehtava.java
@@ -45,6 +45,6 @@ public class Tehtava extends Merkinta {
             muistiinpanot = "-";
         }
 
-        return this.nimi + "\n Aika: " + this.alku.format(formatter) + "\n  Muistiinpanot: " + muistiinpanot + "\n Muistutus: " + muistutus + ;
+        return this.nimi + "\n   Aika: " + this.alku.format(formatter) + "\n  Muistiinpanot: " + muistiinpanot + "\n   Muistutus: " + muistutus;
     }
 }
